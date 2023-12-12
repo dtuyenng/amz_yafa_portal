fdbe75cfb2540fd3a0a926e2f83b56851d911b55
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2023-12-12 12:37:17 -0800

    Commit
    commited

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div id="SearchBar">
+      <input placeholder="Item#"></input>
+      <input placeholder="ASIN"></input>
+      <input placeholder="SKU"></input>
+    </div>
+  );
+}
+
+export default SearchBar;
